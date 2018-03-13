a8b2a3159c32cdf4109e0c8b13f593941b7b3e8f
bpenchas <bpenchas@gmail.com>
2018-03-12 19:52:57 -0700

    starting up

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 import tensorflow as tf
+
+def make_train_dict():
+    train_dict = {}
+    with open("train.vocab") as f:
+        for line in f:
+            (key, val) = line.split(',')
+            train_dict[key] = val
+    return train_dict
+
+if __name__ == "__main__":
+    print("starting")
+    train_dict = make_train_dict()
+    print(train_dict['mano'])
